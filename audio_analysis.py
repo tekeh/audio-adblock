e66fffaac3ed14c4592f4beca16b7a986b97f5fa
Tekeh <te276@cam.ac.uk>
2021-07-02 18:43:03 +0100

    New panel showing the size of displacements in the audio feature space between frames. Also have tweaked the relative size of hop_len and win_len

--- audio_analysis.py
+++ audio_analysis.py
@@ -14,22 +14,24 @@ fname   = './ftnb_2021-06-15.wav'
 y, sr   = load(fname)
 time    = np.arange(0, len(y))/sr
 
+fmax        = 2048
 hop_secs    = 2
-win_secs    = 2
+win_secs    = 4
 hop_length  = int(hop_secs*sr) ## 1 s * sr samples s^-1
 win_length  = int(win_secs*sr)
-mfccs   = mfcc(y,               sr=sr, hop_length=hop_length, n_fft=win_length)
-mels    = melspectrogram(y=y,   sr=sr, hop_length=hop_length, n_fft=win_length)
+mfccs   = mfcc(y,               sr=sr, hop_length=hop_length, window='hann', n_fft=win_length)
+mels    = melspectrogram(y=y,   sr=sr, hop_length=hop_length, window='hann', n_fft=win_length)
+jumps    = np.sum(np.diff(mfccs.T, axis=0)**2, axis=1)
 
 ##### Plot the melspec along with the classifications for visualisation
-fig, (ax0, ax1, ax2) = plt.subplots(3,1, sharex=True)
+fig, (ax0, ax1, ax2, ax3) = plt.subplots(4,1, sharex=True)
 fig.canvas.mpl_connect('close_event', lambda event: play_pod_proc.kill())
 title = ax0.text(0.5, 20.2, "")
 ## MFCCS and Mel Spectrogram 
 #librosa.display.specshow(mfccs, sr=sr, x_coords = time[::hop_length], ax=ax0)
 #librosa.display.specshow(lr.power_to_db(mels,ref=np.max), x_coords=time[::hop_length], y_axis='mel', fmax=4096, ax=ax1)
 ## Clustering  
-n_clusters = 6 ##3 or 4 seems decent
+n_clusters = 3 ##3 or 4 seems decent
 ax1.set_xlim([0, max(time[::hop_length])])
 km = k_means(mfccs.T, n_clusters)
 #ax2.plot(time[::hop_length], km[1])
@@ -39,24 +41,27 @@ km = k_means(mfccs.T, n_clusters)
 
 def init():
     line0, = librosa.display.specshow(mfccs, sr=sr, x_coords = time[::hop_length], ax=ax0)
-    line1, = librosa.display.specshow(power_to_db(mels,ref=np.max), x_coords=time[::hop_length], y_axis='mel', fmax=4096, ax=ax1)
+    line1, = librosa.display.specshow(power_to_db(mels,ref=np.max), x_coords=time[::hop_length], y_axis='mel', fmax=fmax, ax=ax1)
     line2, = ax2.plot(time[::hop_length], km[1])
-    return [line0, line1, line2]
+    line4, = ax3.plot(time[hop_length::hop_length], jumps)
+    return [line0, line1, line2, line4]
 
 def animate(i):
     line0 = librosa.display.specshow(mfccs, sr=sr, x_coords = time[::hop_length], ax=ax0)
-    line1 = librosa.display.specshow(power_to_db(mels,ref=np.max), x_coords=time[::hop_length], y_axis='mel', fmax=4096, ax=ax1)
+    line1 = librosa.display.specshow(power_to_db(mels,ref=np.max), x_coords=time[::hop_length], y_axis='mel', fmax=fmax, ax=ax1)
     
     ax2.cla()
+    ax3.cla()
     line2, = ax2.plot(time[::hop_length], km[1])
     line3, = ax2.plot([i+1,i+1], [0,max(km[1])], 'g')
+    line4, = ax3.plot(time[hop_length::hop_length][1:], jumps[1:])
     title.set_text(f"{i} secs")
     #ax2.figure.canvas.draw()
     if i==1: ## ==0 seems to have wired overlap issue -- investigate
         global play_pod_proc
         play_pod_proc = subprocess.Popen(['cvlc', fname])
 
-    return [line2, line3, title]
+    return [line2, line3, line4, title]
 
 ani = FuncAnimation(fig, animate, interval=1000, blit=True) ## 1 second intervals
 plt.show()
